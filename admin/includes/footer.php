f1134feca9bbada3bd76ae1101c6ed88dae11e99
Orutu  Akposieyefa W <dev@offmyyard.com>
2023-03-12 18:01:57 +0100

    Add your commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>NACOSS</b>
+    </div>
+    <strong> &copy; 2020 Brought To You By<a href="#"> NACOSS ELCOM COMMITEE 2020</a></strong>
+</footer>
